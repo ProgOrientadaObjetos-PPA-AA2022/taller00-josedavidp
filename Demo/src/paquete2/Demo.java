0ffdda688b3f11881c760ec07c3074a1b16ce9b1
josedavidp <jdpiedra1@utpl.edu.ec>
2022-04-14 16:25:13 -0500

    taller00

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -19,7 +19,7 @@ public class Demo {
      */
     public static void main(String[] args) {
 
-       System.out.printf("%s con %d", Mensaje.obtenerFrase(), Valor.obtenerHabitantes());
+       System.out.printf("%s con %d\n", Mensaje.obtenerFrase(), Valor.obtenerHabitantes());
     }
 
 }
